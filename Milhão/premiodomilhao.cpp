2b5607df453b75fa084799113f9890b676308ef9
gabriellucascm <63599233+gabriellucascm@users.noreply.github.com>
2020-10-07 11:07:31 -0300

    Update premiodomilhao.cpp

--- Milhão/premiodomilhao.cpp
+++ Milhão/premiodomilhao.cpp
@@ -1,17 +1,17 @@
-//exercÌco em https://neps.academy/lesson/134
+//exerc√≠cio em https://neps.academy/lesson/134
 
 #include <iostream>
 
 using namespace std;
 
 int main (){
-	int N,dias1=1,dias2=0,A,AC=0;//N È o n˙merod e dias, dias 1 conta os dias atÈ o momento de chegar a um milh„o , dias2 pega o dia certinho que deu 1 milh„o, A pega o valor e AC acumula o A 
+	int N,dias1=1,dias2=0,A,AC=0;//N √© o n√∫mero de dias, dias 1 conta os dias at√© o momento de chegar a um milh√£o , dias2 pega o dia certinho que deu 1 milh√£o, A pega o valor e AC acumula o A 
 	cin >> N;
 	
 	for(N;N>0;N--,dias1++){
 		cin >> A;
 		AC+=A;
-		if(AC>=1000000 && dias2==0)//Verifica se chegou ou n„o ao 1000000
+		if(AC>=1000000 && dias2==0)//Verifica se chegou ou n√£o ao 1000000
 			dias2=dias1;
 	}
 	
